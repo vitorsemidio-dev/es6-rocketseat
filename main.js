aaea4614448d8e446c32e10e46dd7f4aa867b74d
Vitor Emidio <vitorsemidio@gmail.com>
2019-06-29 17:23:29 -0300

    Utilizando classes com heranca e metodo static

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-alert('Teste');
-
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = []
     }
 
-    outro() {
-        
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
+}
+
+
+class TodoList extends List{
+    constructor() {
+        super();
+        this.usuario = 'Emidio';
+    }
+
+    mostrarUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+
+const MinhaList = new TodoList();
+
+document.getElementById('button').onclick = () => {
+    MinhaList.add('Novo todo');
+    MinhaList.mostrarUsuario();
+    console.log(Matematica.soma(1, 15));
 }
