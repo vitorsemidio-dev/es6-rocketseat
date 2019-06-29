56de411ba2dd540ae5535e1717471742178e9215
Vitor Emidio <vitorsemidio@gmail.com>
2019-06-29 18:05:59 -0300

    Desestruturacao de objetos

--- bundle.js
+++ bundle.js
@@ -1,9 +1,33 @@
 "use strict";
 
-var soma = function soma() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-  return a + b;
+var usuario = {
+  nome: 'Emidio',
+  idade: 22,
+  endereco: {
+    cidade: 'Rio de Janeiro',
+    estado: 'RJ'
+  } // #1
+
+};
+var nomeUsuario = usuario.nome;
+var idadeUsuario = usuario.idade;
+var cidadeUsuario = usuario.endereco.cidade;
+console.log(nomeUsuario, idadeUsuario, cidadeUsuario); // #2
+
+var nome = usuario.nome;
+var idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log(nome);
+console.log(idade, cidade);
+
+mostrarNome = function mostrarNome(usuario) {
+  return usuario.nome;
 };
 
-console.log(soma(1, 5));
+mostrarNome2 = function mostrarNome2(_ref) {
+  var nome = _ref.nome;
+  return nome;
+};
+
+console.log(mostrarNome(usuario));
+console.log(mostrarNome2(usuario));
--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-const soma = (a = 0, b = 0) =>  a + b;
+const usuario = {
+    nome: 'Emidio',
+    idade: 22,
+    endereco: {
+        cidade: 'Rio de Janeiro',
+        estado: 'RJ'
+    }
+}
 
-console.log(soma(1, 5));
+// #1
+const nomeUsuario = usuario.nome;
+const idadeUsuario = usuario.idade;
+const cidadeUsuario = usuario.endereco.cidade;
+console.log(nomeUsuario, idadeUsuario, cidadeUsuario);
+
+
+// #2
+const { nome } = usuario;
+const { idade, endereco: { cidade } } = usuario;
+console.log(nome);
+console.log(idade, cidade);
+
+
+
+mostrarNome = (usuario) => usuario.nome;
+mostrarNome2 = ({nome}) => nome;
+console.log(mostrarNome(usuario));
+console.log(mostrarNome2(usuario));
