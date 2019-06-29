be5f7c408b63a6b0932f13f9fe1cc8ff48fb20ba
Vitor Emidio <vitorsemidio@gmail.com>
2019-06-29 17:46:51 -0300

    Retornando array, string e objeto com arraw function

--- bundle.js
+++ bundle.js
@@ -1,23 +1,20 @@
 "use strict";
 
-var arr = [1, 5, 2, 3, 8, 10, 12, 13, 18, 21];
-var arrMap = arr.map(function (value) {
-  return value * 2;
-});
-var arrMapWithIndex = arr.map(function (value, index) {
-  return value * index;
-});
-var sum = arr.reduce(function (total, current) {
-  return total + current;
-});
-var filtro = arr.filter(function (value) {
-  return value % 2 === 0;
-});
-var find = arr.find(function (search) {
-  return search === 11;
-});
-console.log(arrMap);
-console.log(arrMapWithIndex);
-console.log(sum);
-console.log(filtro);
-console.log(find);
+var array = function array() {
+  return [1, 2, 3];
+};
+
+var string = function string() {
+  return 'string';
+};
+
+var objeto = function objeto() {
+  return {
+    tipoRetorno: 'objeto',
+    tipoFuncao: 'Arraw Funciton'
+  };
+};
+
+console.log(array());
+console.log(string());
+console.log(objeto());
--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-const arr = [1, 5, 2, 3, 8, 10, 12, 13, 18, 21]
+const array = () => [1,2,3];
+const string = () => 'string';
+const objeto = () => ({tipoRetorno: 'objeto', tipoFuncao: 'Arraw Funciton'});
 
 
-const arrMap = arr.map((value) => value * 2);
-const arrMapWithIndex = arr.map((value, index) => value * index);
-const sum = arr.reduce((total, current) => total + current);
-const filtro = arr.filter((value) => value % 2 === 0);
-const find = arr.find((search) => search === 11);
-
-console.log(arrMap);
-console.log(arrMapWithIndex);
-console.log(sum);
-console.log(filtro);
-console.log(find);
+console.log(array());
+console.log(string());
+console.log(objeto());
