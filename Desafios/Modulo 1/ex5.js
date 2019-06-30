af2a0781ea996537061bf5f49f6bebc24f545911
Vitor Emidio <vitorsemidio@gmail.com>
2019-06-30 15:57:20 -0300

    Modulo 1 - Exercicio 5 (parte spread)

--- Desafios/Modulo 1/ex5.js
+++ Desafios/Modulo 1/ex5.js
@@ -1,3 +1,5 @@
+// REST
+
 const arr = [1, 2, 3, 4, 5, 6];
 
 const [x, ...y] = arr;
@@ -9,3 +11,28 @@ const soma = (...params) => params.reduce((total, atual) => total + atual);
 
 console.log(soma(1, 2, 3, 4, 5, 6));
 console.log(soma(1, 2));
+
+
+
+// SPREAD
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+        cidade: 'Rio do Sul',
+        uf: 'SC',
+        pais: 'Brasil'
+    }
+}
+
+
+
+const usuario2 = { ...usuario, nome: 'Gabriel' }
+
+const { endereco } = usuario ;
+const newEndereco = { ...endereco, cidade: 'Lontras' }
+const usuario3 = { ...usuario, endereco: newEndereco }
+
+
+console.log(usuario2);
+console.log(usuario3);
