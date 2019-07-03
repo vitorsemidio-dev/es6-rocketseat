f8615079f3eb24125cf6ef69f37ee7ea09572a2c
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-02 22:27:23 -0300

    Mock para adicionar repositorio

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        } catch (err) {
-            console.warn(`Usuario ${username} nao encontrado`);
-        }
+        this.formEl = document.getElementById("repo-form");
 
+        this.registerHandlers();
+    }
+
+
+    // registra os eventos
+    registerHandlers() {
+        this.formEl.onsubmit = evento => this.addRepository(evento);
+    }
+
+
+    addRepository(event) {
+        // previne que os eventos defaults aconteçam (recarregar tela, fazer get dnv)
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'Rocketseat',
+            description: 'ire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/Rocketseat'
+        });
+
+        console.log(this.repositories);
     }
 }
 
-
-Api.getUserInfo('diego3g');
-Api.getUserInfo('vitorsemidio');
+new App();
