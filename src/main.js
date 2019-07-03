6727c2bb266835fbd8c7d9d34233bd4407d99384
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-02 23:09:44 -0300

    merge

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 class App {
     constructor() {
-      this.repositories = [];
-  
-      this.formEl = document.getElementById('repo-form');
-      this.listEl = document.getElementById('repo-list');
-      
-      this.registerListeners();
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById("repo-list");
+
+        this.registerListeners();
     }
   
     registerListeners() {
@@ -14,45 +13,48 @@ class App {
     }
   
     addRepository(event) {
-      event.preventDefault();
-  
-      this.repositories.push({
-        avatar_url: 'https://avatars0.githubusercontent.com/u/28929274',
-        name: 'rocketseat.com.br',
-        description: 'Tire sua ideia do papel e dê vida à sua startup',
-        html_url: 'https://github.com/RocketSeat/rocketseat.com.br',
-      });
   
       this.render();
+        // previne que os eventos defaults aconteçam (recarregar tela, fazer get dnv)
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'Rocketseat',
+            description: 'ire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/Rocketseat'
+        });
+
+        this.render();
     }
-  
+
     render() {
-      this.listEl.innerHTML = '';
-  
-      this.repositories.forEach(repo => {
-        let imgEl = document.createElement('img');
-        imgEl.setAttribute('src', repo.avatar_url);
-        
-        let titleEl = document.createElement('strong');
-        titleEl.appendChild(document.createTextNode(repo.name));
-        
-        let descriptionEl = document.createElement('p');
-        descriptionEl.appendChild(document.createTextNode(repo.description));
-        
-        let linkEl = document.createElement('a');
-          
-        linkEl.appendChild(document.createTextNode('Acessar'));
-        linkEl.setAttribute('target', '_blank');
-  
-        let listItemEl = document.createElement('li');
-  
-        listItemEl.appendChild(imgEl);
-        listItemEl.appendChild(titleEl);
-        listItemEl.appendChild(descriptionEl);
-        listItemEl.appendChild(linkEl);
-  
-        this.listEl.appendChild(listItemEl);
-      });
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createElement(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.appendChild(document.createTextNode(repo.html_url));
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.setAttribute('target', '_blank');
+
+            let listItemEl = document.createElement('li');
+
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
     }
   }
   
